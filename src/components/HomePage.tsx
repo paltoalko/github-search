10dea10cf1de6c85242a0c5d2a60a475f2f36cc5
paltoalko <martas90909@me.com>
2022-12-05 19:29:58 +0000

    integrated search with mock

---
+++ src/components/HomePage.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import styles from '../assets/styles/HomePage.module.css';
+import { Box, Typography } from '@mui/material';
+import SearchBar from './SearchBar';
+import Results from './Results';
+import { searchUsers } from '../api/api';
+import data from '../response.json';
+import { UserType } from '../assets/typings/types';
+
+const Start: React.FC<{}> = () => {
+  const searchResults = data.items;
+  const [searchQuery, setSearchQuery] = useState('');
+  const [users, setUsers] = useState([]);
+
+  const createUsers = (searchResults) => {
+    const newList = searchResults.map((el) => {
+      const user = {
+        id: el.id,
+        name: el.login
+      };
+      return user;
+    });
+
+    setUsers(newList);
+  };
+
+  const handleSearch = (e: string) => {
+    setSearchQuery(e);
+    createUsers(searchResults);
+
+    // searchUsers(e)
+    //   .then((res) => {
+    //     setSearchResults(res);
+    //   })
+    //   .catch((err) => console.log(err));
+  };
+  return (
+    <Box className={styles.homeContainer}>
+      <Typography variant="h1">Github Search</Typography>
+      <SearchBar handleSearch={handleSearch} />
+      <Box sx={{ width: '100%' }}>
+        {searchQuery && <Results searchQuery={searchQuery} users={users} />}
+      </Box>
+    </Box>
+  );
+};
+
+export default Start;
