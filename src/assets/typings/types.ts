f0615a298aa42733e7ee350d17a98c411e1c22e4
paltoalko <martas90909@me.com>
2022-12-05 19:30:03 +0000

    typings

---
+++ src/assets/typings/types.ts
@@ -0,0 +1,12 @@
+export type UserType = {
+  id: number;
+  name: string;
+};
+
+export type RepoType = {
+  name: string;
+  description: string;
+  stars: number;
+  url: string;
+  id: number;
+};
