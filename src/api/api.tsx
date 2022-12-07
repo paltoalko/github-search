746b9006ee70ea962dfe6bbb05d91b3b880baeed
paltoalko <martas90909@me.com>
2022-12-07 13:05:59 +0000

    check reauest function added

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,12 +1,20 @@
+const initial_url = 'https://api.github.com';
+
 export const searchUsers = async (name: string) => {
-  const data = await fetch(`https://api.github.com/search/users?q=${name}&per_page=5`);
+  const data = await fetch(`${initial_url}/search/users?q=${name}&per_page=5`);
   const result = await data.json();
 
   return result.items;
 };
 
 export const getReposList = async (name: string) => {
-  const data = await fetch(`https://api.github.com/users/${name}/repos`);
+  const data = await fetch(`${initial_url}/users/${name}/repos`);
   const result = await data.json();
   return result;
+};
+
+export const checkRequest = async () => {
+  const data = await fetch(`${initial_url}/rate_limit`);
+  const result = await data.json();
+  return result.rate;
 };
