00c76c988038f5af781a4c64662a87fccb7e1238
paltoalko <martas90909@me.com>
2022-12-07 13:06:33 +0000

    fixed api and added checking requests

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,46 +1,83 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from '../assets/styles/HomePage.module.css';
-import { Box, Typography } from '@mui/material';
+import { Box, CircularProgress, Typography } from '@mui/material';
 import SearchBar from './SearchBar';
 import Results from './Results';
-import data from '../response.json';
-import { searchUsers } from '../api/api';
+import { checkRequest, searchUsers } from '../api/api';
 import { UserType } from '../assets/typings/types';
 
-const Start: React.FC<{}> = () => {
-  const searchResults = data.items;
+const HomePage: React.FC<{}> = () => {
   const [searchQuery, setSearchQuery] = useState('');
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState<UserType[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [remainingRequests, setRemainingRequests] = useState('');
+  const [date, setDate] = useState('');
+
+  useEffect(() => {
+    checkRequest()
+      .then((res) => {
+        checkAvailableRequests(res);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const checkAvailableRequests = (res) => {
+    setRemainingRequests(res.remaining);
+    const newDate = new Date(res.reset).toTimeString();
+    setDate(newDate);
+  };
 
   const createUsers = (searchResults) => {
     const newList = searchResults.map((el) => {
       const user = {
         id: el.id,
-        name: el.login
+        name: el.login,
+        profileUrl: el.html_url
       };
       return user;
     });
+    setLoading(false);
     setUsers(newList);
   };
 
-  const handleSearch = (e) => {
+  const handleSearch = (e: string) => {
+    setLoading(true);
     setSearchQuery(e);
-    createUsers(searchResults);
-    // searchUsers(e)
-    //   .then((res) => {
-    //     setSearchResults(res);
-    //   })
-    //   .catch((err) => console.log(err));
+    searchUsers(e)
+      .then((res) => {
+        if (res.length === 0) {
+          createUsers([]);
+        } else {
+          createUsers(res);
+        }
+      })
+      .catch((err) => console.log(err));
   };
   return (
     <Box className={styles.homeContainer}>
-      <Typography variant="h1">Github Search</Typography>
-      <SearchBar handleSearch={handleSearch} />
-      <Box sx={{ width: '100%' }}>
-        {searchQuery && <Results searchQuery={searchQuery} users={users} />}
+      <Box className={styles.homeBox}>
+        <Typography variant="h1">Github Search</Typography>
+        <SearchBar handleSearch={handleSearch} />
+        <Box sx={{ width: '100%' }}>
+          {searchQuery && (
+            <Results
+              searchQuery={searchQuery}
+              users={users}
+              checkAvailableRequests={checkAvailableRequests}
+            />
+          )}
+          {loading && (
+            <Box className={styles.progress}>
+              <CircularProgress />
+            </Box>
+          )}
+        </Box>
       </Box>
+      <Typography className={styles.remainingText} variant="body2" fontWeight={200}>
+        Remaining requests {remainingRequests}/60. Resets on {date}.
+      </Typography>
     </Box>
   );
 };
 
-export default Start;
+export default HomePage;
--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from '../assets/styles/Results.module.css';
 import {
   Box,
@@ -10,50 +10,76 @@ import {
   Collapse,
   Paper,
   Badge,
-  ListItem
+  ListItem,
+  CircularProgress
 } from '@mui/material';
-import { RepoType, UserType } from '../assets/typings/types';
+import { RepoType, UserComponent, UserType } from '../assets/typings/types';
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 import { ExpandLess, ExpandMore, StarOutlined } from '@mui/icons-material';
-import { getReposList } from '../api/api';
-import data from '../reposResponse.json';
+import { checkRequest, getReposList } from '../api/api';
 
 interface IProps {
   users?: UserType[] | undefined;
   searchQuery?: string;
+  checkAvailableRequests: (res) => void;
 }
 
-const UserElement: React.FC<UserType> = ({ id, name }) => {
+const UserElement: React.FC<UserComponent> = ({ id, name, profileUrl, checkAvailableRequests }) => {
   const [openRepos, setOpenRepos] = useState(false);
   const [userRepos, setUserRepos] = useState<RepoType[]>([]);
+  const [noRepos, setNoRepos] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    checkRequest()
+      .then((res) => {
+        checkAvailableRequests(res);
+      })
+      .catch((err) => console.log(err));
+  }, [openRepos]);
 
   const createRepos = (repos) => {
-    const newRepos = repos.map((el) => {
-      const repo = {
-        name: el.name,
-        description: el.description,
-        stars: el.stargazers_count,
-        url: el.html_url,
-        id: el.id
-      };
-      return repo;
-    });
-    setUserRepos(newRepos);
+    setLoading(false);
+    if (repos.length == 0) {
+      setNoRepos(true);
+    } else {
+      setNoRepos(false);
+      const newRepos = repos.map((el) => {
+        const repo = {
+          name: el.name,
+          description: el.description,
+          stars: el.stargazers_count,
+          url: el.html_url,
+          id: el.id
+        };
+        return repo;
+      });
+
+      setUserRepos(newRepos);
+    }
   };
 
   const handleClick = () => {
-    // getReposList(name);
-    createRepos(data);
+    if (openRepos == false) {
+      setLoading(true);
+      getReposList(name)
+        .then((res) => {
+          createRepos(res);
+        })
+        .catch((err) => console.log(err));
+    }
+
     setOpenRepos(!openRepos);
-
-    console.log(userRepos);
-
-    //get available repos from that person
   };
   return (
     <Paper sx={{ mb: '0.5em' }} className={styles.listItem}>
-      <ListItemButton key={id} onClick={handleClick}>
-        <ListItemIcon>
+      <ListItemButton key={id} onClick={handleClick} aria-label="User repositories">
+        <ListItemIcon
+          aria-label="User profile link"
+          onClick={(e) => {
+            e.preventDefault();
+            window.open(profileUrl, '_blank')?.focus();
+          }}>
           <AccountCircleOutlinedIcon />
         </ListItemIcon>
         <ListItemText primary={name} />
@@ -66,6 +92,7 @@ const UserElement: React.FC<UserType> = ({ id, name }) => {
               <Box sx={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
                 <ListItemText
                   disableTypography
+                  aria-label="User repository link"
                   onClick={(e) => {
                     e.preventDefault();
                     window.open(el.url, '_blank')?.focus();
@@ -96,12 +123,51 @@ const UserElement: React.FC<UserType> = ({ id, name }) => {
             </ListItem>
           </List>
         ))}
+        {noRepos && (
+          <List component="div" disablePadding>
+            <ListItem sx={{ pl: 4 }}>
+              <Box sx={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
+                <ListItemText
+                  disableTypography
+                  primary={
+                    <Typography variant="body1" color="primary" sx={{ cursor: 'pointer' }}>
+                      This user doesn&apos;t have any repositories.
+                    </Typography>
+                  }
+                />
+              </Box>
+            </ListItem>
+          </List>
+        )}
+        {loading && (
+          <List component="div" disablePadding>
+            <ListItem sx={{ p: 4 }}>
+              <Box
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  width: '100%',
+                  justifyConten: 'center',
+                  alignItems: 'center'
+                }}>
+                <CircularProgress />
+              </Box>
+            </ListItem>
+          </List>
+        )}
       </Collapse>
     </Paper>
   );
 };
 
-const Results: React.FC<IProps> = ({ users, searchQuery }) => {
+const Results: React.FC<IProps> = ({ users, searchQuery, checkAvailableRequests }) => {
+  useEffect(() => {
+    checkRequest()
+      .then((res) => {
+        checkAvailableRequests(res);
+      })
+      .catch((err) => console.log(err));
+  }, []);
   return (
     <Box className={styles.resultsContainer}>
       <Box className={styles.resultsBox}>
@@ -111,14 +177,42 @@ const Results: React.FC<IProps> = ({ users, searchQuery }) => {
           Showing users for &apos;{searchQuery}&apos;
         </Typography>
 
-        <List
-          className={styles.searchResults}
-          aria-labelledby="results-nested-list"
-          sx={{ width: '70vw' }}>
-          {users?.map((user) => (
-            <UserElement key={user.id} id={user.id} name={user.name} />
-          ))}
-        </List>
+        {users.length > 0 && (
+          <List
+            className={styles.searchResults}
+            aria-labelledby="results-nested-list"
+            sx={{ width: '70vw' }}>
+            {users?.map((user) => (
+              <UserElement
+                key={user.id}
+                id={user.id}
+                name={user.name}
+                profileUrl={user.profileUrl}
+                checkAvailableRequests={checkAvailableRequests}
+              />
+            ))}
+          </List>
+        )}
+
+        {users.length == 0 && (
+          <Box className={styles.searchResults} sx={{ width: '70vw' }}>
+            <Typography variant="body1" sx={{ fontSize: '1.5em' }} fontWeight={500} color="error">
+              No users were found.
+            </Typography>
+            <Typography variant="body1" fontWeight={500} color="error">
+              Try searching for another username.
+            </Typography>
+            <Typography variant="body1" fontWeight={400}>
+              Remember that username:{' '}
+            </Typography>
+            <Typography variant="body2" fontWeight={300}>
+              - May only contain alphanumeric characters or hyphens. <br />
+              - Cannot have multiple consecutive hyphens. <br />
+              - Cannot begin or end with a hyphen.
+              <br />- Maximum is 39 characters.
+            </Typography>
+          </Box>
+        )}
       </Box>
     </Box>
   );
