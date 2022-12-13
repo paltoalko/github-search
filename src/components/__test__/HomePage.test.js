4d7afe87d32c4f7834ce7f53ccbe2df2de8a86de
paltoalko <martas90909@me.com>
2022-12-13 21:03:42 +0100

    added mock test

--- src/components/__test__/HomePage.test.js
+++ src/components/__test__/HomePage.test.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-undef */
 import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import HomePage from '../HomePage';
+import { getReposList } from '../../api/api';
 
 describe('Home Page', () => {
   it('should render the query search', async () => {
@@ -16,5 +17,20 @@ describe('Home Page', () => {
       const divElement = screen.getByRole('heading', { name: /showing users for 'palto'/i });
       expect(divElement).toBeInTheDocument();
     });
+  });
+  it('returns a list of repositories for the given user', async () => {
+    const mockFetch = jest.fn(() =>
+      Promise.resolve({
+        json: () => Promise.resolve([{ name: 'Repo 1' }, { name: 'Repo 2' }])
+      })
+    );
+    global.fetch = mockFetch;
+    const result = await getReposList('fake-user-name');
+
+    expect(mockFetch).toHaveBeenCalledWith('https://api.github.com/users/fake-user-name/repos');
+    expect(result).toBeInstanceOf(Array);
+    expect(result.length).toBe(2);
+    expect(result[0]).toHaveProperty('name', 'Repo 1');
+    expect(result[1]).toHaveProperty('name', 'Repo 2');
   });
 });
